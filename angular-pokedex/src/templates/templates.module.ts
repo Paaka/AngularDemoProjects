46156277d2fdc19920a355d203cf6d96834255f5
Paaka <32105930+Paaka@users.noreply.github.com>
2021-12-02 22:55:46 +0100

    feat(globally): added routing

---
+++ angular-pokedex/src/templates/templates.module.ts
@@ -0,0 +1,7 @@
+import {NgModule} from '@angular/core';
+import { LoadingTemplateComponent } from './loading-template/loading-template.component';
+
+@NgModule({
+  declarations: [LoadingTemplateComponent]
+})
+export class TemplatesModule { }
